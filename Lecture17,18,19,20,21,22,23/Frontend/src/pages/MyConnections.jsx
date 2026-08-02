f183001dc6a94bea20ec96e5592ec1d77415005e
Mayank Ahuja <mayankahuja700@gmail.com>
2026-08-02 21:50:32 +0530

    Update MyConnections.jsx

--- Lecture17,18,19,20,21,22,23/Frontend/src/pages/MyConnections.jsx
+++ Lecture17,18,19,20,21,22,23/Frontend/src/pages/MyConnections.jsx
@@ -66,7 +66,7 @@ function MyConnections(){
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {connections.map((connection) => {
             const otherUser =
-              connection.sender._id === user?._id
+              connection.sender._id === user?.userId
                 ? connection.receiver
                 : connection.sender;
 
